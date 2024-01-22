d07d680d6402b23a146db9330bacb9cf5f760f42
Rushikesh-wagh <111699859+Rishi-Wagh@users.noreply.github.com>
2024-01-22 23:06:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(errorHandler);
 //     res.send("<h1>Hello I am from your backend about</h1>");
 //   });
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log("listening on"+PORT+"...");
